7adb429692a3f83bcb868a8f8d2493cfc870026f
Jenny Chandler <bk22551@bristol.ac.uk>
2022-11-16 11:04:29 +0000

    corrected author

--- ca_fire_growth_experiment.py
+++ ca_fire_growth_experiment.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Oct  4 20:35:51 2022
+Created on Wed Nov  16 11:01:51 2022
 
-@author: steph
+@author: Jenny Chandler
 """
 from ca_fire import evolve_forest, write_netcdf
 
